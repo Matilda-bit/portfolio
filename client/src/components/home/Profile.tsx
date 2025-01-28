a415babb3a2f8948a5eece53e909ff9dbb712e15
Matilda-bit <polinaovras@gmail.com>
2025-01-28 03:27:57 +0200

    added btns  download resume and contact me

--- client/src/components/home/Profile.tsx
+++ client/src/components/home/Profile.tsx
@@ -11,25 +11,27 @@ const Profile: React.FC = () => {
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
+                        <div className='colz-icon'>
+                            <a href='https://www.facebook.com/ovras94/' target="_blank" rel="noopener noreferrer">
+                                <i className='fa fa-facebook'></i>
+                            </a>
+                            <a href="https://www.linkedin.com/in/polina-ovras/" target="_blank" rel="noopener noreferrer">
+                                <i className='fa fa-linkedin'></i>
+                            </a>
+                            <a href='https://github.com/Matilda-bit' target="_blank" rel="noopener noreferrer">
+                                <i className='fa fa-github'></i>
+                            </a>
+                            <a href='https://api.whatsapp.com/send/?phone=%2B972543954881&text&type=phone_number&app_absent=0' target="_blank" rel="noopener noreferrer">
+                                <i className='fa fa-whatsapp'></i>
+                            </a>
+                            <a href="mailto:polinaovras@gmail.com?subject=subject&message=message" target="_blank" rel="noopener noreferrer">
+                                <i className='fa fa-google'></i>
+                            </a>
+                            <a href='https://www.instagram.com/ovraspolina?igsh=Ymh5MTJjbmx2Zncw&utm_source=qr' target="_blank" rel="noopener noreferrer">
+                                <i className='fa fa-instagram'></i>
+                            </a>
+                        </div>
 
-                        <a href='https://www.facebook.com/ovras94/' target="_blank" rel="noopener noreferrer">
-                            <i className='fa fa-facebook'></i>
-                        </a>
-                        <a href="https://www.linkedin.com/in/polina-ovras/" target="_blank" rel="noopener noreferrer">
-                            <i className='fa fa-linkedin'></i>
-                        </a>
-                        <a href='https://github.com/Matilda-bit' target="_blank" rel="noopener noreferrer">
-                            <i className='fa fa-github'></i>
-                        </a>
-                        <a href='https://api.whatsapp.com/send/?phone=%2B972543954881&text&type=phone_number&app_absent=0' target="_blank" rel="noopener noreferrer">
-                            <i className='fa fa-whatsapp'></i>
-                        </a>
-                        <a href="mailto:polinaovras@gmail.com?subject=subject&message=message" target="_blank" rel="noopener noreferrer">
-                            <i className='fa fa-google'></i>
-                        </a>
-                        <a href='https://www.instagram.com/ovraspolina?igsh=Ymh5MTJjbmx2Zncw&utm_source=qr' target="_blank" rel="noopener noreferrer">
-                            <i className='fa fa-instagram'></i>
-                        </a>
                     </div>
 
 
@@ -48,31 +50,44 @@ const Profile: React.FC = () => {
                                 <Typical
                                     loop={Infinity}
                                     steps={[
-                                        "Software Engineer",//17 chars
+                                        "Software Engineer 🎓",//17 chars
                                         2000,
-                                        "FullStack Dev",//13
+                                        "FullStack Dev 🦆",//13
                                         2000,
-                                        "Frontend Dev",//12
+                                        "Frontend Dev 🎨",//12
                                         2000,
-                                        "TypeScript",//12
+                                        "TypeScript 🔧",//12
                                         2000,
-                                        "React",
+                                        "React 🪄",
+                                        1500,
+                                        "JS + CSS 📟",
+                                        1600,
+                                        "PHP 🐘",
+                                        1300,
+                                        "Node.js 🗿",
                                         1400,
-                                        "PHP",
-                                        1400,
+                                        "Passionate about learning and growing! ✓✓✓",//38
+                                        4000
                                     ]}
-
                                     wrapper='p'
                                 />
-
                             </h1>
+                            <span className='profile-role-tagline'> Experienced in crafting innovative solutions using advanced front-end and back-end technologies, with a strong focus on detail and quality.</span>
                         </span>
                     </div>
+                    <div className='profile-options'>
+                        <button className='btn-hire-me btn primary-btn'>Hire Me</button>
+                        <a href='PolinaOvras.pdf' download='Polina Ovras.pdf'>
+                            <button className='btn btn-resume highlighted-btn'>Get Resume</button>
+                        </a>
+                    </div>
                 </div>
+                <div className='profile-picture'>
+                    <div className='profile-picture-bg'>
 
-
+                    </div>
+                </div>
             </div>
-
         </div>
     );
 }
